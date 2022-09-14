3e05b85dd7bd3909a0231a4ffe8b641a1a618a0e
kms825252 <kms825252@gmail.com>
2022-09-14 23:37:52 +0900

    0914

--- 22_09/BOJ/1747.py
+++ 22_09/BOJ/1747.py
@@ -2,33 +2,24 @@ import math
 import sys
 
 def isPrime(n):
-    if n ==1:
-        return False
-
     for i in range(2, int(math.sqrt(n))+1):
         if n % i == 0:
             return False
     return True
 
 num = int(sys.stdin.readline())
-flag = 1
 
-while True:
-    if isPrime(num):
-        num = str(num)
-        for i in range(len(num)//2):
-            if num[i] != num[-i-1]:
-                flag = 0
-                break
+a= 0
+for i in range(num, 1000001):
+    if i == 1:
+        continue
+    i = str(i)
+    if i == i[::-1]:
+        i= int(i)
+        if isPrime(i):
+            a = i
+            break
 
-
-
-print(num)
-
-# n = 1000000
-# arr = [1] * n
-#
-# for i in range(2, int(math.sqrt(n))+1):
-#     if arr[i] == 1:
-#         j = 2
-#         while i*j <=n:
+if a == 0:
+    a = 1003001
+print(a)
