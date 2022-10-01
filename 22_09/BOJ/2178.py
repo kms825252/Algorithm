24c91f428a2b61fb0e4d344afc7cb47c19f72db2
kms825252 <kms825252@gmail.com>
2022-10-02 00:01:41 +0900

    1001

--- 22_09/BOJ/2178.py
+++ 22_09/BOJ/2178.py
@@ -1,6 +1,27 @@
-def dfs(x,y):
-    case[x][y] = 0
+from collections import deque
 
+def bfs(x,y):
+    di = [0, 1, 0, -1]
+    dj = [1, 0, -1 ,0]
 
-N, M = map(int, input().split())
-case = [list(map(int, input().split())) for _ in range(N)]
+    queue = deque()
+    queue.append((x,y))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            ni = x + di[i]
+            nj = y + dj[i]
+
+            if 0 <= ni < n and 0 <= nj < m:
+                if arr[ni][nj] == 1:
+                    queue.append((ni,nj))
+                    arr[ni][nj] = arr[x][y] + 1
+
+    return arr[n-1][m-1]
+
+n, m = map(int, input().split())
+arr = [list(map(int, input())) for _ in range(n)]
+
+print(bfs(0,0))
