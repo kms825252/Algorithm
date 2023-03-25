7ed9e06df4cd8a06d2c1d8dd2632e5513644163e
kms825252 <kms825252@gmail.com>
2023-03-26 00:42:38 +0900

    0326

---
+++ 23_03/BOJ/11057.py
@@ -0,0 +1,5 @@
+n = int(input())
+
+for i in range(10**n):
+
+    for j in str(i):
---
+++ 23_03/BOJ/1138.py
@@ -0,0 +1,16 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+order = [0] * len(arr)
+
+for i in range(len(arr)):
+    cnt = 0
+    for j in range(len(order)):
+        if order[j] == 0:
+            cnt +=1
+            if cnt == arr[i] + 1:
+                order[j] = i+1
+                break
+
+for k in order:
+    print(k, end=' ')
