c435fe7d3c2a23781b7e0a0478ca50f244b50e9e
kms825252 <kms825252@gmail.com>
2023-07-15 17:21:15 +0900

    0715

---
+++ 23_07/BOJ/11729.py
@@ -0,0 +1,13 @@
+def sol(n, start, end):
+    if n == 1:
+        print(start, end)
+        return
+
+    sol(n-1, start, 6-start-end)
+    print(start, end)
+    sol(n-1, 6-start-end, end)
+
+
+n = int(input())
+print(2**n-1)
+sol(n, 1, 3)
